d1563640a1343119585329ab8614fbc5d1fbeaf4
Alex Speranza <alex.speranza@studio.unibo.it>
2021-12-16 12:12:26 +0100

    feat(git): add git log parser

---
+++ source/glparser.d
@@ -0,0 +1,65 @@
+module glparser;
+
+GitCommitSummary[] getCommitSummaries()
+{
+    import std.process : executeShell;
+    import std.stdio : stderr;
+    import std.string : split, chomp;
+    import std.algorithm.iteration : map;
+    import std.array : array;
+    import core.stdc.stdlib : exit;
+
+    auto gitLogList = executeShell("git log --pretty=\"%s\"");
+    if (gitLogList.status > 0)
+    {
+        stderr.writeln(gitLogList.output);
+        return exit(gitLogList.status);
+    }
+    else
+    {
+        return gitLogList.output.chomp.split("\n").map!(s => parseGitCommitLine(s)).array;
+    }
+}
+
+private GitCommitSummary parseGitCommitLine(string commitString)
+{
+    import std.regex : regex, matchFirst;
+    import std.stdio : writeln;
+    import std.string : strip;
+
+    auto headerRegex = regex(r"^\w+");
+    auto scopeRegex = regex(r"(?<=\()\w+(?=\))");
+    auto messageRegex = regex(r"(?<=:).*");
+    auto headerMatch = commitString.matchFirst(headerRegex);
+    string header = headerMatch.hit.strip;
+    auto scopeMatch = headerMatch.post.matchFirst(scopeRegex);
+    string zcope = scopeMatch.empty ? "" : scopeMatch.hit.strip;
+    auto messageMatch = (scopeMatch.empty ? headerMatch.post : scopeMatch.post).matchFirst(
+            messageRegex);
+    string message = messageMatch.hit.strip;
+    return scopeMatch.empty ? GitCommitSummary(header,
+            message) : GitCommitSummary(header, zcope, message);
+}
+
+struct GitCommitSummary
+{
+    import std.typecons : Nullable;
+
+    string type;
+    Nullable!string typeScope;
+    string message;
+
+    this(string type, string typeScope, string message) @safe nothrow pure @nogc
+    {
+        this.type = type;
+        this.typeScope = typeScope;
+        this.message = message;
+    }
+
+    this(string type, string message) @safe nothrow pure @nogc
+    {
+        this.type = type;
+        this.typeScope = Nullable!string();
+        this.message = message;
+    }
+}
