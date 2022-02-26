c4851288761c00130f20fabcb5b36e579f009fdc
Alex Speranza <alex.speranza@studio.unibo.it>
2022-02-26 15:32:31 +0100

    refactor: merge the subcommand package and configuration module, by also adding the usage of a recursive OptionParser for the main program

--- source/configuration.d
+++ source/configuration.d
@@ -1,54 +1,93 @@
 module configuration;
 
 import std.stdio : File, stdout, stderr;
-import subcommand;
+import asperan.cli_args.option_parser : Subcommand, CommandLineOptionParser;
+import asperan.cli_args.recursive_option_parser : RecursiveOptionParserBuilder;
+
+import subcommand.commit;
+import subcommand.changelog;
+import subcommand.tag;
+
+// This array is kept to generate easily the subcommand help message.
+private Subcommand[] subcommands;
+private CommandLineOptionParser mainOptionParser;
+
+static this() {
+    import std.algorithm.iteration : each;
+
+    subcommands = [
+        new Commit(),
+        new Changelog(), 
+        new Tag(),
+    ];
+
+    RecursiveOptionParserBuilder parserBuilder =  new RecursiveOptionParserBuilder()
+        .addOption("-h", "--help", "Print help message and exit", () { printHelpAndExit(); });
+
+    subcommands.each!(s => parserBuilder.addSubcommand(s));
+
+    mainOptionParser = parserBuilder.build();
+}
 
 void parseArguments(string[] args)
 {
-    if (args.length == 0)
+    auto parseResult = mainOptionParser.parse(args);
+    if (parseResult.subcommand.isNull)
     {
-        stderr.writeln("No subcommand specified");
+        stderr.writeln("No recognized subcommand has been specified");
         printHelpAndExit(1, stderr);
     }
     else
     {
-        runCommandBySmallestUniquePrefix(args[0], args[1 .. $]);
-    }
-}
-
-private void runCommandBySmallestUniquePrefix(string subcommandPrefix, string[] subcommandArgs)
-{
-    import std.algorithm.searching : startsWith;
-    import std.algorithm.iteration : filter;
-    import std.array : array;
-
-    string[] prefixedCommands = subcommands.keys.filter!(s => s.startsWith(subcommandPrefix)).array;
-
-    if (prefixedCommands.length <= 0) // no subcommand with prefix
-    {
-        stderr.writeln("No subcommand with prefix '" ~ subcommandPrefix ~ "'");
-        printHelpAndExit(1, stderr);
-    }
-    else if (prefixedCommands.length >= 2) // More than one subcommand with the prefix
-    {
-        stderr.writeln("More than one subcommand with prefix '" ~ subcommandPrefix ~ "'");
-        printHelpAndExit(1, stderr);
-    }
-    else
-    {
-        SubCommand subcommand = subcommands[prefixedCommands[0]];
-        subcommand.parseOptions(subcommandArgs);
-        subcommand.run();
+        parseResult.subcommand.get.run(parseResult.remainingArguments);
     }
 }
 
 private void printHelpAndExit(uint exitStatus = 0, File sink = stdout)
 {
-    import subcommand;
     import core.stdc.stdlib : exit;
 
-    string usageInfo = "Usage: degast <subcommand> [subcommand-options]";
+    string usageInfo = "Usage: degast <subcommand> [subcommand-options]" ~ "\n";
+    string optionsInfo = "Options:\n" ~ getOptionHelpMessage();
     string subcommandsInfo = "Subcommands:\n" ~ getSubCommandHelpMessage();
-    sink.writeln(usageInfo ~ "\n" ~ subcommandsInfo);
+    sink.writeln(usageInfo ~ "\n" ~ optionsInfo ~ "\n" ~ subcommandsInfo);
     exit(exitStatus);
+}
+
+private string getOptionHelpMessage()
+{
+    import asperan.cli_args.option_parser : CommandLineOption;
+    import std.algorithm.iteration : map, reduce;
+    import std.algorithm.searching : maxElement;
+    import std.string : leftJustify;
+
+    CommandLineOption[] options = mainOptionParser.getOptions;
+    size_t maxShortVersionLength = options.map!(o => o.shortName.length).maxElement;
+    size_t maxLongVersionLength = options.map!(o => o.longName.length).maxElement;
+    return options
+            .map!(
+                o => 
+                    " " ~
+                    leftJustify(o.shortName, maxShortVersionLength) ~ 
+                    " " ~
+                    leftJustify(o.longName, maxLongVersionLength) ~
+                    " " ~
+                    o.description ~
+                    "\n"
+            )
+            .reduce!"a ~ b";
+}
+
+private string getSubCommandHelpMessage()
+{
+    import std.algorithm.iteration : map;
+    import std.algorithm.searching : maxElement;
+    import std.string : leftJustify, join;
+
+    size_t maxCommandLength = subcommands.map!(s => s.getName.length).maxElement;
+    // dfmt off
+    return subcommands
+            .map!(k => " - " ~ k.getName.leftJustify(maxCommandLength, ' ') ~ " => " ~ k.getDescription)
+            .join("\n");
+    // dfmt on
 }
